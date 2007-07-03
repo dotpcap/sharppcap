648465ac2798a1c0cfcde47e5fec51fe9a9651f9
tamirgal <tamirgal@d0cf58d0-740f-40d2-b45d-ea2607ed902b>
2007-07-03 10:30:07 +0000

    This commit was generated by cvs2svn to compensate for changes in r7, which included commits to RCS files with non-trunk default branches.

--- Examples/Example1.IfList/AssemblyInfo.cs
+++ Examples/Example1.IfList/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
+//
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
