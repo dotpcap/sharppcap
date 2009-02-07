e9b758ecc0e6fc73fb786093ec120da86b203228
chrismorgan <chrismorgan@d0cf58d0-740f-40d2-b45d-ea2607ed902b>
2009-02-07 17:08:54 +0000

    Remove unneeded IPAddressList class.

--- Test/IPSubnetTest.cs
+++ Test/IPSubnetTest.cs
@@ -15,28 +15,11 @@ namespace Test
 			Console.WriteLine(ipRange.NetworkAddress);
 			Console.WriteLine(ipRange.BroadcastAddress);
 			ipRange.setRandom(false);
-			int count = (int)ipRange.size();
-			
-			ArrayList list = new ArrayList();
 
-			
-			foreach(String ip in ipRange)
-			{
-				//System.Console.Out.WriteLine(ip);
-				list.Add(ip);
-				if (--count == 0)
-					break;
-			}
-
-			IPAddressList ipList = new IPAddressList(list);
-			ipList.setRandom(false);
-			count = (int)ipList.size();
-			foreach(String ip in ipList)
-			{
-				System.Console.Out.WriteLine(ip);
-				if (--count == 0)
-					break;
-			}
+            foreach(System.Net.IPAddress ip in ipRange)
+            {
+				System.Console.Out.WriteLine(ip);                
+            }
 		}
 	}
 }
