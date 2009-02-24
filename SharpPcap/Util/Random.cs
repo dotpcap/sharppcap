af30af85b739db2fe9cef5dab33c706fb13a01a8
chrismorgan <chrismorgan@d0cf58d0-740f-40d2-b45d-ea2607ed902b>
2009-02-24 17:49:33 +0000

    Add some Random generator methods

--- SharpPcap/Util/Random.cs
+++ SharpPcap/Util/Random.cs
@@ -50,9 +50,34 @@ namespace SharpPcap.Util
 
         public double GetDouble()
         {
+#if false
             byte[] bytes = GetBytes(8);
             double d = System.BitConverter.ToDouble(bytes, 0);
             return d;
+#else
+            return (new Random(GetInt())).NextDouble();
+#endif
+        }
+
+        public long GetLong()
+        {
+            byte[] bytes = GetBytes(8);
+            long i = System.BitConverter.ToInt32(bytes, 0);
+            return i;
+        }
+
+        public long GetLong(long min, long max)
+        {
+            double _min = min;
+            double _max = max;
+            double dif = (_max - _min + 1);
+            double final = ((GetDouble() * dif) + _min);
+            return (long)final;
+        }
+
+        public long GetLong(long max)
+        {
+            return GetLong(0, max);
         }
 
         public int GetInt()
@@ -60,6 +85,16 @@ namespace SharpPcap.Util
             byte[] bytes = GetBytes(4);
             int i = System.BitConverter.ToInt32(bytes, 0);
             return i;
+        }
+
+        public int GetInt(int min, int max)
+        {
+            return (int)GetLong(min, max);
+        }
+
+        public int GetInt(int max)
+        {
+            return (int)GetLong(0, max);
         }
     }
 }
