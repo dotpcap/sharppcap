20228483bc1189ca6cc4d972f8fd67ab0d875cf7
chrismorgan <chrismorgan@d0cf58d0-740f-40d2-b45d-ea2607ed902b>
2009-02-25 23:15:26 +0000

    Add in a capture file that contains a single tcp packet and check in the improvements to TCPPacketTest.cs

--- Test/TCPPacketTest.cs
+++ Test/TCPPacketTest.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using SharpPcap.Packets;
 
 namespace SharpPcap.Util
 {
@@ -11,19 +12,59 @@ namespace SharpPcap.Util
         public virtual void  SetUp()
         {
         }
-        
-        [Test]
-        public virtual void  testSubnet()
-        {
-            try
-            {
 
-            }
-            catch (System.Exception e)
-            {
-                Console.Error.WriteLine(e.StackTrace);              
-                Assert.Fail(e.ToString());
-            }
+        [Test]
+        public virtual void Checksum()
+        {
+            PcapOfflineDevice dev = Pcap.GetPcapOfflineDevice("../../capture_files/tcp.pcap");
+            dev.PcapOpen();
+
+            Packet p;
+            p = dev.PcapGetNextPacket();
+
+            Assert.IsNotNull(p);
+
+            Console.WriteLine(p.GetType());
+            Assert.IsTrue(p is TCPPacket);
+
+            TCPPacket t = (TCPPacket)p;
+
+            Console.WriteLine("Checksum: "+t.Checksum.ToString("X"));
+            Assert.IsTrue(t.ValidChecksum);
+
+            dev.PcapClose();
+        }
+
+        // Test that we can load and parse an IPv6 TCP packet
+        [Test]
+        public void IPDataSet()
+        {
+            TCPPacket p = TCPPacket.RandomPacket(IPPacket.IPVersions.IPv4);
+
+            p.IPTotalLength = 100;
+            Assert.AreEqual(100, p.IPTotalLength);
+            Assert.AreEqual(80, p.IPPayloadLength);
+
+            p.IPPayloadLength = 200;
+            Assert.AreEqual(200, p.IPPayloadLength);
+            Assert.AreEqual(220, p.IPTotalLength);
+        }
+
+        // Test that we can load and parse an IPv6 TCP packet
+        [Test]
+        public void TCPDataIPv4()
+        {
+            String s = "-++++=== HELLLLOOO ===++++-";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(s);
+
+            //create random pkt
+            TCPPacket p = TCPPacket.RandomPacket(IPPacket.IPVersions.IPv4);
+
+            //replace pkt's data with our string
+            p.TCPData = data;
+
+            //sanity check
+            Assert.AreEqual(s, System.Text.Encoding.Default.GetString(p.TCPData));
         }
     }
 }
