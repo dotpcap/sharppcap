41a4ebd3d87efd1e4a0b929f8acc703431b08150
Chris Morgan <chmorgan@gmail.com>
2009-09-11 09:59:19 -0400

    Begin clarifying the license as LGPL

--- SharpPcap/PcapDevice.cs
+++ SharpPcap/PcapDevice.cs
@@ -1,30 +1,24 @@
 /*
-Copyright (c) 2005 Tamir Gal, http://www.tamirgal.com, All rights reserved.
+This file is part of SharpPcap.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+SharpPcap is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    1. Redistributions of source code must retain the above copyright notice,
-        this list of conditions and the following disclaimer.
+SharpPcap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-    2. Redistributions in binary form must reproduce the above copyright 
-        notice, this list of conditions and the following disclaimer in 
-        the documentation and/or other materials provided with the distribution.
-
-    3. The names of the authors may not be used to endorse or promote products
-        derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
-OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+You should have received a copy of the GNU Lesser General Public License
+along with SharpPcap.  If not, see <http://www.gnu.org/licenses/>.
 */
+/* 
+ * Copyright 2005 Tamir Gal <tamir@tamirgal.com>
+ * Copyright 2008-2009 Chris Morgan <chmorgan@gmail.com>
+ * Copyright 2008-2009 Phillip Lemon <lucidcomms@gmail.com>
+ */
 
 using System;
 using System.Text;
