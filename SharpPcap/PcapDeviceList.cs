be55e3e7b7327a9c5998fd7b662cfd8e88c86508
Chris Morgan <chmorgan@gmail.com>
2009-11-30 18:59:18 -0500

    Remove unneeded System.Linq using statement

--- SharpPcap/PcapDeviceList.cs
+++ SharpPcap/PcapDeviceList.cs
@@ -25,7 +25,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
-using System.Linq;
 using SharpPcap.Containers;
 
 namespace SharpPcap
