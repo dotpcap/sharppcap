5b0e4c14baafe55b4d178bcb5e121af421168c8a
Chris Morgan <chmorgan@gmail.com>
2010-02-28 00:54:33 -0500

    Bump assembly version to 3.0 in preparation for releasing the Packet.Net version of SharpPcap

--- SharpPcap/AssemblyInfo.cs
+++ SharpPcap/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyVersion("3.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
