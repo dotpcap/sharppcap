af54faedbf030012cbb2ef8be6b10e8f439e5642
Chris Morgan <chmorgan@gmail.com>
2011-01-01 23:16:58 -0500

    Convert tabs to spaces in AirPcapUnmanagedStructures

--- SharpPcap/AirPcap/AirPcapUnmanagedStructures.cs
+++ SharpPcap/AirPcap/AirPcapUnmanagedStructures.cs
@@ -35,21 +35,21 @@ namespace SharpPcap.AirPcap
             ///Channel frequency, in MHz
             ///UINT
             ///</summary>
-	        public UInt32 Frequency;
+            public UInt32 Frequency;
 
             /// <summary>
             /// 802.11n specific. Offset of the extension channel in case of 40MHz channels. 
-		    ///
-		    /// Possible values are -1, 0 +1: 
-		    /// - -1 means that the extension channel should be below the control channel (e.g. Control = 5 and Extension = 1)
-		    /// - 0 means that no extension channel should be used (20MHz channels or legacy mode)
-		    /// - +1 means that the extension channel should be above the control channel (e.g. Control = 1 and Extension = 5)
-		    ///
-		    /// In case of 802.11a/b/g channels (802.11n legacy mode), this field should be set to 0.
+            ///
+            /// Possible values are -1, 0 +1:
+            /// - -1 means that the extension channel should be below the control channel (e.g. Control = 5 and Extension = 1)
+            /// - 0 means that no extension channel should be used (20MHz channels or legacy mode)
+            /// - +1 means that the extension channel should be above the control channel (e.g. Control = 1 and Extension = 5)
+            ///
+            /// In case of 802.11a/b/g channels (802.11n legacy mode), this field should be set to 0.
             ///
             /// CHAR
             /// </summary>
-	        public sbyte ExtChannel;
+            public sbyte ExtChannel;
 
             /// <summary>
             /// Channel Flags. The only flag supported at this time is \ref AIRPCAP_CIF_TX_ENABLED.
@@ -60,7 +60,7 @@ namespace SharpPcap.AirPcap
             /// <summary>
             /// Reserved. It should be set to {0,0}.
             /// </summary>
-	        byte Reserved1;
+            byte Reserved1;
             byte Reserved2;
         };
 
@@ -75,25 +75,25 @@ namespace SharpPcap.AirPcap
             /// from the beginning of the current capture. This value includes the packets 
             /// dropped because of buffer full.
             ///</summary>
-	        public UInt32 /* UINT */ Recvs;
+            public UInt32 /* UINT */ Recvs;
 
             ///<summary>
             /// Number of packets that the driver dropped from the beginning of a capture.
             /// A packet is lost when the the buffer of the driver is full. 
             /// </summary>
-	        public UInt32 /* UINT */ Drops;
+            public UInt32 /* UINT */ Drops;
 
             /// <summary>
             /// Packets dropped by the card before going to the USB bus. 
             /// Not supported at the moment.
             /// </summary>
-	        public UInt32 /* UINT */ IfDrops;
+            public UInt32 /* UINT */ IfDrops;
 
             /// <summary>
             /// Number of packets that pass the BPF filter, find place in the kernel buffer and
             /// therefore reach the application.
             /// </summary>
-	        public UInt32 /* UINT */ Capt;
+            public UInt32 /* UINT */ Capt;
         };
 
         /// <summary>
@@ -106,23 +106,23 @@ namespace SharpPcap.AirPcap
             /// <summary>
             /// An id that identifies the adapter model
             /// </summary>
-	        public AirPcapAdapterId AdapterId;
+            public AirPcapAdapterId AdapterId;
             /// <summary>
             /// String containing a printable adapter model
             /// </summary>
-	        public string /* CHAR* */ AdapterModelName;
+            public string /* CHAR* */ AdapterModelName;
             /// <summary>
             /// The type of bus the adapter is plugged to
             /// </summary>
-	        public AirPcapAdapterBus AdapterBus;
+            public AirPcapAdapterBus AdapterBus;
             /// <summary>
             /// TRUE if the adapter is able to perform frame injection.
             /// </summary>
-	        public bool CanTransmit;
+            public bool CanTransmit;
             /// <summary>
             /// TRUE if the adapter's transmit power is can be specified by the user application.
             /// </summary>
-	        public bool CanSetTransmitPower;
+            public bool CanSetTransmitPower;
             /// <summary>
             /// TRUE if the adapter supports plugging one or more external antennas.
             /// </summary>
@@ -132,12 +132,12 @@ namespace SharpPcap.AirPcap
             /// \ref AIRPCAP_MEDIUM_802_11_B, \ref AIRPCAP_MEDIUM_802_11_G or \ref AIRPCAP_MEDIUM_802_11_N.
             /// Not supported at the moment.
             /// </summary>
-	        public UInt32 /* UINT */ SupportedMedia;
+            public UInt32 /* UINT */ SupportedMedia;
             /// <summary>
             /// An OR combination of the bands that the device supports. Can be one of: \ref AIRPCAP_BAND_2GHZ, 
             /// \ref AIRPCAP_BAND_5GHZ.
             /// </summary>
-	        public UInt32 /* UINT */ SupportedBands;
+            public UInt32 /* UINT */ SupportedBands;
         }
 
 
@@ -152,18 +152,18 @@ namespace SharpPcap.AirPcap
             /// <summary>
             /// Type of key, can be on of: \ref AIRPCAP_KEYTYPE_WEP, \ref AIRPCAP_KEYTYPE_TKIP, \ref AIRPCAP_KEYTYPE_CCMP. Only AIRPCAP_KEYTYPE_WEP is supported by the driver at the moment.
             /// </summary>
-	        public AirPcapKeyType /* UINT */ KeyType;
+            public AirPcapKeyType /* UINT */ KeyType;
 
             /// <summary>
             /// Length of the key in bytes
             /// </summary>
-	        public UInt32 /* UINT */ KeyLen;
+            public UInt32 /* UINT */ KeyLen;
 
             /// <summary>
             /// Key data
             /// </summary>
             [MarshalAs(UnmanagedType.ByValArray, SizeConst=WepKeyMaxSize)]
-	        public byte[] /* BYTE */ KeyData;
+            public byte[] /* BYTE */ KeyData;
         }
 
         /// <summary>
@@ -214,13 +214,13 @@ namespace SharpPcap.AirPcap
             /// <summary>
             /// Number of keys in the collection
             /// </summary>
-	        public UInt32 /* UINT */ nKeys;
+            public UInt32 /* UINT */ nKeys;
 
 #if false
             /// <summary>
             /// Array of nKeys keys.
             /// </summary>
-	        /* AirpcapKey Keys[0]; */
+            /* AirpcapKey Keys[0]; */
             public AirpcapKey[] keys;
 #endif
         };
@@ -285,24 +285,24 @@ namespace SharpPcap.AirPcap
         [StructLayout(LayoutKind.Sequential)]
         internal struct ieee80211_radiotap_header
         {
-            public Byte it_version;	/* Version 0. Only increases
-					                 * for drastic changes,
-					                 * introduction of compatible
-					                 * new fields does not count.
-					                 */
+            public Byte it_version; /* Version 0. Only increases
+                                     * for drastic changes,
+                                     * introduction of compatible
+                                     * new fields does not count.
+                                     */
             public Byte it_pad;
             public UInt16 it_len;   /* length of the whole
-					                 * header in bytes, including
-					                 * it_version, it_pad,
-					                 * it_len, and data fields.
-					                 */
+                                     * header in bytes, including
+                                     * it_version, it_pad,
+                                     * it_len, and data fields.
+                                     */
             public UInt32 it_present;  /* A bitmap telling which
-					                    * fields are present. Set bit 31
-					                    * (0x80000000) to extend the
-					                    * bitmap by another 32 bits.
-					                    * Additional extensions are made
-					                    * by setting bit 31.
-					                    */
+                                        * fields are present. Set bit 31
+                                        * (0x80000000) to extend the
+                                        * bitmap by another 32 bits.
+                                        * Additional extensions are made
+                                        * by setting bit 31.
+                                        */
         };
     }
 }
