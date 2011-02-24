df6a8b4e76535d40be8b622ea604f7f248d3f652
Chris Morgan <chmorgan@gmail.com>
2011-02-23 21:31:41 -0500

    Update assembly version from 3.5.0 to 3.6.0

--- SharpPcap/AssemblyInfo.cs
+++ SharpPcap/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.5.0")]
+[assembly: AssemblyVersion("3.6.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
