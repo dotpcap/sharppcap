e574f6f39a64de0f35b30b7042d473e79ec40f63
Chris Morgan <chmorgan@gmail.com>
2011-09-08 20:04:09 -0400

    Add unit tests for ICaptureDevice vs. a specfic device type and for unsafe code

--- Test/Performance/PacketParsing.cs
+++ Test/Performance/PacketParsing.cs
@@ -8,15 +8,88 @@ namespace Test.Performance
     [TestFixture]
     public class PacketParsing
     {
+        private int packetsToRead = 50000000;
+
         [Test]
         public void Benchmark()
         {
-            int packetsToRead = 50000000;
             int packetsRead = 0;
             var startTime = DateTime.Now;
             while(packetsRead < packetsToRead)
             {
                 var captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
+                captureDevice.Open();
+
+                RawCapture rawCapture = null;
+                do
+                {
+                    rawCapture = captureDevice.GetNextPacket();
+
+                    // Parse the packet using PacketDotNet
+                    if(rawCapture != null)
+                        Packet.ParsePacket(rawCapture.LinkLayerType, rawCapture.Data);
+
+                    packetsRead++;
+                }
+                while(rawCapture != null);
+
+                captureDevice.Close();
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, packetsRead, "packets parsed");
+
+            Console.WriteLine("{0}", rate.ToString());
+        }
+
+        /// <summary>
+        /// Test the performance of a device used via the ICaptureDevice interface
+        /// </summary>
+        [Test]
+        public void BenchmarkICaptureDevice()
+        {
+            int packetsRead = 0;
+            var startTime = DateTime.Now;
+            while(packetsRead < packetsToRead)
+            {
+                ICaptureDevice captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
+                captureDevice.Open();
+
+                RawCapture rawCapture = null;
+                do
+                {
+                    rawCapture = captureDevice.GetNextPacket();
+
+                    // Parse the packet using PacketDotNet
+                    if(rawCapture != null)
+                        Packet.ParsePacket(rawCapture.LinkLayerType, rawCapture.Data);
+
+                    packetsRead++;
+                }
+                while(rawCapture != null);
+
+                captureDevice.Close();
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, packetsRead, "packets parsed");
+
+            Console.WriteLine("{0}", rate.ToString());
+        }
+
+        /// <summary>
+        /// Test the performance of a device used via the ICaptureDevice interface
+        /// </summary>
+        [Test]
+        public unsafe void BenchmarkICaptureDeviceUnsafe()
+        {
+            int packetsRead = 0;
+            var startTime = DateTime.Now;
+            while(packetsRead < packetsToRead)
+            {
+                ICaptureDevice captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
                 captureDevice.Open();
 
                 RawCapture rawCapture = null;
--- Test/Performance/PacketReading.cs
+++ Test/Performance/PacketReading.cs
@@ -7,15 +7,72 @@ namespace Test.Performance
     [TestFixture]
     public class PacketReading
     {
+        private int packetsToRead = 50000000;
+
         [Test]
         public void Benchmark()
         {
-            int packetsToRead = 50000000;
             int packetsRead = 0;
             var startTime = DateTime.Now;
             while(packetsRead < packetsToRead)
             {
                 var captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
+                captureDevice.Open();
+
+                RawCapture rawCapture = null;
+                do
+                {
+                    rawCapture = captureDevice.GetNextPacket();
+                    packetsRead++;
+                }
+                while(rawCapture != null);
+
+                captureDevice.Close();
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, packetsRead, "packets captured");
+
+            Console.WriteLine("{0}", rate.ToString());
+        }
+
+        [Test]
+        public void BenchmarkICaptureDevice()
+        {
+            int packetsRead = 0;
+            var startTime = DateTime.Now;
+            while(packetsRead < packetsToRead)
+            {
+                ICaptureDevice captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
+                captureDevice.Open();
+
+                RawCapture rawCapture = null;
+                do
+                {
+                    rawCapture = captureDevice.GetNextPacket();
+                    packetsRead++;
+                }
+                while(rawCapture != null);
+
+                captureDevice.Close();
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, packetsRead, "packets captured");
+
+            Console.WriteLine("{0}", rate.ToString());
+        }
+
+        [Test]
+        public unsafe void BenchmarkICaptureDeviceUnsafe()
+        {
+            int packetsRead = 0;
+            var startTime = DateTime.Now;
+            while(packetsRead < packetsToRead)
+            {
+                ICaptureDevice captureDevice = new SharpPcap.LibPcap.CaptureFileReaderDevice("../../capture_files/10k_packets.pcap");
                 captureDevice.Open();
 
                 RawCapture rawCapture = null;
