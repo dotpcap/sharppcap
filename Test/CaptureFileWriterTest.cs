aa6b0a63f2dccd6b7e10adc0e9eecefbd43d699b
Chris Morgan <chmorgan@gmail.com>
2021-01-24 15:42:37 -0500

    CaptureFileWriterTest - Add additional test cases to improve code coverage

--- Test/CaptureFileWriterTest.cs
+++ Test/CaptureFileWriterTest.cs
@@ -1,4 +1,6 @@
+using System;
 using NUnit.Framework;
+using SharpPcap;
 using SharpPcap.LibPcap;
 
 namespace Test
@@ -6,6 +8,8 @@ namespace Test
     [TestFixture]
     public class CaptureFileWriterTest
     {
+        static string filename = @"abc.pcap";
+
         public CaptureFileWriterTest()
         {
         }
@@ -17,11 +21,56 @@ namespace Test
         [Test]
         public void TestFileCreationAndDeletion()
         {
-            using (var wd = new CaptureFileWriterDevice(@"abc.pcap"))
+            using (var wd = new CaptureFileWriterDevice(filename))
             {
-                wd.Write(new byte[] { 1, 2, 3, 4 });
+                Assert.AreEqual(filename, wd.Name);
+                Assert.IsNotEmpty(wd.Description);
+                var bytes = new byte[] { 1, 2, 3, 4 };
+                wd.Write(bytes);
+
+                var p = new RawCapture(PacketDotNet.LinkLayers.Ethernet, new PosixTimeval(), bytes);
+                wd.Write(p);
             }
             System.IO.File.Delete(@"abc.pcap");
+        }
+
+        [Test]
+        public void TestCreationOptions()
+        {
+            // valid arguments results in the object being created
+            using var valid = new CaptureFileWriterDevice(PacketDotNet.LinkLayers.Ethernet, null, "somefilename.pcap", System.IO.FileMode.Open);
+
+            // invalid snapshot length should throw
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                using var wd = new CaptureFileWriterDevice(PacketDotNet.LinkLayers.Ethernet, 500000, "somefilename.pcap", System.IO.FileMode.Open);
+            });
+
+            // file mode of append should throw
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                using var wd = new CaptureFileWriterDevice(PacketDotNet.LinkLayers.Ethernet, null, "somefilename.pcap", System.IO.FileMode.Append);
+            });
+        }
+
+        [Test]
+        public void SendPacket()
+        {
+            var bytes = new byte[] { 0x10, 0x20, 0x30 };
+
+            using (var wd = new CaptureFileWriterDevice(filename))
+            {
+                Assert.Throws<NotSupportedOnCaptureFileException>(() => wd.SendPacket(bytes));
+            }
+        }
+
+        [Test]
+        public void Statistics()
+        {
+            using (var wd = new CaptureFileWriterDevice(filename))
+            {
+                Assert.Throws<NotSupportedOnCaptureFileException>(() => { var s = wd.Statistics; });
+            }
         }
     }
 }
