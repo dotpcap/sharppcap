37c6db057999a71fbe17395524cff52ee38502e8
Chris Morgan <chmorgan@gmail.com>
2021-01-23 21:47:00 -0500

    SharpPcap - Expose internals to 'Test' namespace. Add a test for PcapException to improve code coverage.

---
+++ Test/PcapExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using SharpPcap;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class PcapExceptionTest
+    {
+        [Test]
+        public void Exception()
+        {
+            var ex = new PcapException();
+        }
+    }
+}
