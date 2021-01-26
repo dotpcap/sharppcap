f5fd44949510be031d3429dfff5e212505bcd0eb
Chris Morgan <chmorgan@gmail.com>
2021-01-26 18:17:38 -0500

    Test - Add instantiation of some exceptions to increase code coverage

---
+++ Test/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SharpPcap;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Exceptions
+    {
+        public Exceptions()
+        {
+            var deviceNotReadyException = new DeviceNotReadyException();
+            var statistics = new StatisticsException("test description");
+            var npcapRequiredException = new NpcapRequiredException("test message");
+        }
+    }
+}
