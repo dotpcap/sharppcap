adada9c01aa204374a791c2b0a381d040fd0771f
Chris Morgan <chmorgan@gmail.com>
2021-01-27 18:53:01 -0500

    ArpTest - Tests for ARP class

---
+++ Test/ArpTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace Test
+{
+    [TestFixture]
+    public class ArpTest
+    {
+        [Test]
+        public void TestArp()
+        {
+            var d = TestHelper.GetPcapDevice();
+            var arp = new ARP(d);
+
+            // timeout should not be null
+            Assert.IsNotNull(arp.Timeout);
+
+            // and we can set a timeout
+            arp.Timeout = new TimeSpan(0, 0, 2);
+
+            var destinationIP = new System.Net.IPAddress(new byte[] { 8, 8, 8, 8 });
+
+            // Note: We don't care about the success or failure here
+            arp.Resolve(destinationIP);
+        }
+    }
+}
