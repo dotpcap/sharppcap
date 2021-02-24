938ea47f0e109b3210233f9b31a556a507a3fa58
Chris Morgan <chmorgan@gmail.com>
2021-02-23 21:36:35 -0500

    IpHelperTest - Test fixture for IpHelper class

---
+++ Test/WinDivert/IpHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using NUnit.Framework;
+using SharpPcap.WinDivert;
+
+namespace Test.WinDivert
+{
+    [TestFixture]
+    [Category("WinDivert")]
+    [Platform("Win", Reason = "IpHelper.dll is only available for Windows")]
+    public class IpHelperTest
+    {
+        [Test]
+        public void Test()
+        {
+            var localhost = IPAddress.Parse("127.0.0.1");
+            var bestInterface = IpHelper.GetBestInterface(localhost);
+            Assert.IsNotNull(bestInterface);
+
+            var external = IPAddress.Parse("8.8.8.8");
+            var bestInterfaceIndex = IpHelper.GetBestInterfaceIndex(external);
+            Assert.IsFalse(IpHelper.IsOutbound(bestInterfaceIndex, external, localhost));
+            Assert.IsFalse(IpHelper.IsOutbound(bestInterfaceIndex, localhost, external));
+        }
+    }
+}
