4a8b5be7b2b27030dd68b3c758a25d7e9df9e00d
Chris Morgan <chmorgan@gmail.com>
2021-04-26 13:49:35 -0400

    Add ICaptureHeader to IInjectionDevice, some devices, such as WinDivertDevice, require a header to send a packet

--- SharpPcap/IInjectionDevice.cs
+++ SharpPcap/IInjectionDevice.cs
@@ -23,20 +23,19 @@ namespace SharpPcap
 {
     public interface IInjectionDevice : IPcapDevice
     {
-
         /// <summary>
         /// Sends a raw packet through this device
         /// </summary>
         /// <param name="p">The packet to send</param>
         /// <param name="size">The number of bytes to send</param>
-        void SendPacket(RawCapture p);
+        void SendPacket(RawCapture p, ICaptureHeader header = null);
 
         /// <summary>
         /// Sends a raw packet through this device
         /// </summary>
         /// <param name="p">The packet bytes to send</param>
         /// <param name="size">The number of bytes to send</param>
-        void SendPacket(ReadOnlySpan<byte> p);
+        void SendPacket(ReadOnlySpan<byte> p, ICaptureHeader header = null);
     }
 }
 
