c5b080866d768e68e4c8207c22959c3a57786e01
Ayoub Kaanich <kayoub5@live.com>
2021-09-15 22:49:00 +0200

    Add support for WinpkFilter

---
+++ SharpPcap/WinpkFilter/HardwarePacketFilters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SharpPcap.WinpkFilter
+{
+
+    /// <summary>
+    /// The NDIS_PACKET_TYPE enum.
+    /// See https://docs.microsoft.com/en-us/windows-hardware/drivers/network/oid-gen-current-packet-filter
+    /// See https://www.ntkernel.com/docs/windows-packet-filter-documentation/ndisapi-c-2/sethwpacketfilter/
+    /// </summary>
+    [Flags]
+    public enum HardwarePacketFilters : uint
+    {
+        /// <summary>
+        /// Used to reset the default filter
+        /// </summary>
+        None = 0x0000,
+        /// <summary>
+        /// Only packets directed to the workstation’s adapter are accepted
+        /// </summary>
+        Directed = 0x0001,
+        /// <summary>
+        /// Only the multicast packets belonging to the groups of which this adapter is a member are accepted.
+        /// </summary>
+        Multicast = 0x0002,
+        /// <summary>
+        /// Every multicast packet is accepted.
+        /// </summary>
+        AllMulticast = 0x0004,
+        /// <summary>
+        /// Only the broadcast packets are accepted.
+        /// </summary>
+        Broadcast = 0x0008,
+        /// <summary>
+        /// The promiscuous mode. Every incoming packet is accepted by the adapter.
+        /// </summary>
+        Promiscuous = 0x0020,
+        /// <summary>
+        /// All local packets, i.e. Directed + Broadcast + Multicast
+        /// </summary>
+        AllLocal = 0x0080,
+    }
+
+}
---
+++ SharpPcap/WinpkFilter/IntermediateBuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpPcap.WinpkFilter
+{
+
+    /// <summary>
+    /// Contains packet both metadata and data
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal unsafe struct IntermediateBuffer
+    {
+        internal IntermediateBufferHeader Header;
+        internal fixed byte Frame[NativeMethods.MAX_ETHER_FRAME];
+    }
+
+    /// <summary>
+    /// Contains packet NDIS flags and WinPkFilter specific flags.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal struct IntermediateBufferHeader
+    {
+        #region LIST_ENTRY members
+        internal IntPtr Flink;
+        internal IntPtr Blink;
+        #endregion
+
+        internal PacketSource Source;
+        internal uint Length;
+        internal uint Flags;
+        internal uint Dot1q;
+        internal uint FilterId;
+
+        internal uint Reserved0;
+        internal uint Reserved1;
+        internal uint Reserved2;
+        internal uint Reserved3;
+    }
+
+}
