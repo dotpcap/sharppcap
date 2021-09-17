9ed2f13cce238fa7838dc911be58a1ef0c42ef21
Ayoub Kaanich <kayoub5@live.com>
2021-09-17 22:45:14 +0200

    Unit Tests
    Update install-winpkfilter.ps1
    
    Change install mode
    
    Remove DriverVersion
    
    Use web instead of ping
    
    Don't use WinpkFilter on circleci
    
    No WinpkFilter on old appveyor

--- Test/TestHelper.cs
+++ Test/TestHelper.cs
@@ -12,11 +12,13 @@ using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Net;
 
 namespace Test
 {
     internal static class TestHelper
     {
+
         public static string GetFile(string name)
         {
             var assembly = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -92,7 +94,7 @@ namespace Test
                 sender.Open(mode, 1);
             }
             PacketCapture e;
-            using(sender)
+            using (sender)
             {
                 routine(sender);
                 // waiting for any queued packets to be sent
