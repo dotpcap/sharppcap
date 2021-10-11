79de277b8b8b9663ff84fdb9fd27699c20624066
Ayoub Kaanich <kayoub5@live.com>
2021-10-11 20:33:44 +0200

    Linux Tap Support

---
+++ SharpPcap/Tunneling/WinTapHeader.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SharpPcap.Tunneling
+{
+    public class TunnelHeader : ICaptureHeader
+    {
+        public PosixTimeval Timeval { get; set; }
+
+        public TunnelHeader()
+        {
+            Timeval = new PosixTimeval();
+        }
+    }
+}
