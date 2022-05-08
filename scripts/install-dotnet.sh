2fffed1cb0bdd9c79234f834858c09a754df5335
Ayoub Kaanich <kayoub5@live.com>
2022-05-08 12:46:59 +0200

    Install in /usr/local/bin

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 5.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 5.0 --install-dir /usr/local/bin
