ae1b0b2c0b4f432772b85bf0f980e253ef6e07bf
Ayoub Kaanich <kayoub5@live.com>
2022-05-27 09:00:45 +0200

    Debug appveyor failure

--- scripts/install-winpkfilter.ps1
+++ scripts/install-winpkfilter.ps1
@@ -2,8 +2,10 @@
 Push-Location $env:TEMP
 
 $arch = If ([Environment]::Is64BitOperatingSystem) {'x64'} Else {'x86'}
+$url = "https://www.ntkernel.com/downloads/Windows Packet Filter 3.2.29.1 $arch.msi"
 
-Invoke-WebRequest "https://www.ntkernel.com/downloads/Windows Packet Filter 3.2.29.1%20$arch.msi" -OutFile "WinpkFilter-$arch.msi"
+echo "Downloading $url"
+Invoke-WebRequest $url -OutFile "WinpkFilter-$arch.msi"
 Start-Process "WinpkFilter-$arch.msi" -ArgumentList "/norestart /quiet /l WinpkFilter-$arch.log" -wait
 type "WinpkFilter-$arch.log"
 
