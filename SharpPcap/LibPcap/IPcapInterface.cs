3f107666311dcffda61480d9a9010977d513feff
Iris van de Zandschulp <iris.van.de.zandschulp@lansweeper.com>
2022-08-23 15:50:18 +0200

    feat: extracted interface

---
+++ SharpPcap/LibPcap/IPcapInterface.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace SharpPcap.LibPcap
+{
+    public interface IPcapInterface
+    {
+        /// <value>
+        /// Name of the interface. Used internally when passed to pcap_open_live()
+        /// </value>
+        string Name { get; }
+
+        /// <value>
+        /// Human readable interface name derived from System.Net.NetworkInformation.NetworkInterface.Name
+        /// </value>
+        string FriendlyName { get; }
+
+        /// <value>
+        /// Text description of the interface as given by pcap/npcap
+        /// </value>
+        string Description { get; }
+
+        /// <value>
+        /// Gateway address of this device
+        /// NOTE: May only be available on Windows
+        /// </value>
+        List<IPAddress> GatewayAddresses { get; }
+
+        /// <value>
+        /// Addresses associated with this device
+        /// </value>
+        List<PcapAddress> Addresses { get; }
+
+        /// <value>
+        /// Pcap interface flags
+        /// </value>
+        uint Flags { get; }
+
+        /// <summary>
+        /// MacAddress of the interface
+        /// </summary>
+        PhysicalAddress MacAddress { get; }
+
+        /// <summary>
+        /// Timestamps supported by this device
+        /// </summary>
+        ///
+        /// <remarks>
+        /// Note: Live devices can have supported timestamps but offline devices
+        /// (such as file readers etc) do not. See https://www.tcpdump.org/manpages/pcap-tstamp.7.html
+        /// </remarks>
+        System.Collections.Generic.List<PcapClock> TimestampsSupported { get; }
+
+        /// <summary>
+        /// ToString override
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/>
+        /// </returns>
+        string ToString();
+    }
+}
