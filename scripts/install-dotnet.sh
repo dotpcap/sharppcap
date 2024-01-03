ce285ad5705bef734dfc528a9386b6663337374b
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2024-01-03 22:35:57 +0100

    Clean dotnet install logic

--- scripts/install-dotnet.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 6.0 "$@"
-
-dotnet --list-sdks
--- scripts/install-windows.ps1
+++ scripts/install-windows.ps1
@@ -1,4 +1,3 @@
 
-choco install -y dotnet-sdk --version=6.0.300
 choco install -y tapwindows
 choco install -y procdump
